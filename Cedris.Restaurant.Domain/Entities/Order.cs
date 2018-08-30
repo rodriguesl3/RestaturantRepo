517e2602dbb31ee56ca788408f1b44791068cdb4
Lucas Rodrigues <lucas.souza@indigosoft.com>
2018-08-29 22:37:01 -0300

    Added domain entities

--- Cedris.Restaurant.Domain/Entities/Order.cs
+++ Cedris.Restaurant.Domain/Entities/Order.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace Cedris.Restaurant.Domain.Entities
 {
-    public class Origem
+    public class Order
     {
+        public Order ()
+        {
+            Id = Guid.NewGuid();
+        }     
+
+        public Guid Id{get;set;}
+        public List<Item> Items { get; set; }
+        public Table Table { get; set; }
+
+
     }
 }
--- Cedris.Restaurant.Domain/Entities/Table.cs
+++ Cedris.Restaurant.Domain/Entities/Table.cs
@@ -2,16 +2,16 @@ using System;
 
 namespace Cedris.Restaurant.Domain.Entities
 {
-    public class Item
+    public class Table
     {
-        public Item()
+        public Table()
         {
             Id = Guid.NewGuid();
         }
 
         public Guid Id { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
-        public string Image { get; set; }
+        public int NumberAlias { get; set; }
+
     }
 }
