8cb8866dbaf4b5d280633f1e75ecbfb29818eb4a
Lucas Rodrigues <lucas.souza@indigosoft.com>
2018-08-30 11:00:41 -0300

    Added angular material in frontend project

--- APP/restaurant-app/src/app/app.module.ts
+++ APP/restaurant-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MatButtonModule} from '@angular/material/button';
+
+
 import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { TablesComponent } from './tables/tables.component';
 import { ItemsComponent } from './items/items.component';
+
 import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +28,8 @@ import { MainComponent } from './main/main.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
